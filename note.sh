2fd16d2d4ab2e50a93156a280892e45c98a56fa9
J. Taylor O'Connor <taylor@singlemusic.co>
2019-01-11 20:53:01 -0600

    add rm command

--- note.sh
+++ note.sh
@@ -2,7 +2,8 @@ function mdless() {
     pandoc -s -f markdown -t man $1 | groff -T utf8 -man | less
 }
 
-function _noteedit() {
+# open a note in vim
+function _note_edit() {
     if [ $# -eq 0 ]; then
         echo "Please provide a filename argument"
         return 1
@@ -11,8 +12,8 @@ function _noteedit() {
     vim ~/.notes/"$1.md";
 }
 
-function _notecode() {
-    # TODO: Refactor this ish to a function
+# open a note in vs code
+function _note_code() {
     if [ $# -eq 0 ]; then
         echo "Please provide a filename argument"
         return  1
@@ -21,25 +22,37 @@ function _notecode() {
 	code ~/.notes/"$1.md";
 }
 
-function _notels() {
+function _note_ls() {
     ls -1 ~/.notes/*.md
+}
+
+function _note_rm() {
+    read -r -p "Are you sure? [y/N] " response
+    case "$response" in
+        [yY][eE][sS]|[yY]) 
+            rm ~/.notes/"$1.md"
+            echo "removed note: $1"
+        ;;
+    esac
 }
 
 function note() {
     if [ $# -eq 0 ]; then
-        # TODO: Usage function
         echo "Please provide a command or filename"
         return 1
     fi
     case $1 in
         "code")
-            _notecode $2
+            _note_code $2
         ;;
         "ls")
-            _notels
+            _note_ls
+        ;;
+        "rm")
+            _note_rm $2
         ;;
         "edit")
-            _noteedit $2
+            _note_edit $2
         ;;
         *)
             mdless ~/.notes/"$1.md";
